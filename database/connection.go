86152e6e1055f08c03ca8a1d8dca4d0023c0611b
Uduak <Lorduduak@gmail.com>
2023-10-18 02:29:09 +0100

    Update connection.go

--- database/connection.go
+++ database/connection.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	conn, err := gorm.Open(mysql.Open("mysql://uduak:quixote4@tcp(provisioning:3306)/primarydb?charset=utf8&parseTime=True&loc=Local"), &gorm.Config{})
+	conn, err := gorm.Open(mysql.Open("mysql://uduak:quixote4@provisioning:3306/primarydb"), &gorm.Config{})
     if err != nil {
         panic("Could not connect to database")
     }
